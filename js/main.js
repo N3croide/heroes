d74c0e39bffd4e3cb7d33d123eece5477819aed5
N3croide <106851453+N3croide@users.noreply.github.com>
2024-03-01 09:48:29 -0500

    :sparkles: Se agrega el modal

--- js/main.js
+++ js/main.js
@@ -22,31 +22,71 @@ const fragment = document.createDocumentFragment();
 let doc = document.getElementById("mainContainer");
 
 let tarjetasHeroes = (marvelOrDc) => heroes[marvelOrDc].forEach( element => {
-      let tarjeta = document.createElement("div");
-      let img = document.createElement("img");
-      let nombreHero = document.createElement("div");
-      let boton = document.createElement("input");
-      let logo = document.getElementById("logo");
+  let tarjeta = document.createElement("div");
+  let img = document.createElement("img");
+  let nombreHero = document.createElement("div");
+  let boton = document.createElement("input");
+  let logo = document.getElementById("logo");
 
-      tarjeta.className = "tarjetas";
-      img.src = `${element["picture"]}`;
-      logo.src =(marvelOrDc == "dc") ? "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/800px-DC_Comics_logo.svg.png" : "https://seeklogo.com/images/M/Marvel_Comics-logo-D489AEB9C1-seeklogo.com.png";
-      logo.animate([{transform: 'rotate(0deg) scale(0) '},{transform: 'rotate(3630deg) scale(1) ' }],{duration: 400});
-      nombreHero.className = "hoverTarjeta"
-      nombreHero.textContent = `${element["name"]}`;
-      
-      boton.type = "button";
-      boton.value = "VER";
-      boton.className = "botonTarjetas";
+  tarjeta.className = "tarjetas";
+  img.src = `${element["picture"]}`;
+  logo.src =(marvelOrDc == "dc") ? "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/800px-DC_Comics_logo.svg.png" : "https://seeklogo.com/images/M/Marvel_Comics-logo-D489AEB9C1-seeklogo.com.png";
+  logo.animate([{transform: 'rotate(0deg) scale(0) '},{transform: 'rotate(3630deg) scale(1) ' }],{duration: 400});
+  nombreHero.className = "hoverTarjeta"
+  nombreHero.textContent = `${element["name"]}`;
+  
+  boton.type = "button";
+  boton.value = "VER";
+  boton.className = "botonTarjetas";
+  boton.id = `boton${element["name"].replace(/ /g,'')}`;
+  boton.addEventListener('click',()=>{
+    let fragmentoModal = document.createDocumentFragment();
 
-      tarjeta.appendChild(img);
-      nombreHero.appendChild(boton);
-      tarjeta.appendChild(nombreHero);
-      // estructura div.tarjeta > img > div.nombreHero > input.button
+    let divModal = document.getElementById("modal");
+    let divContainerModal = document.createElement("div");
+    let spanModal = document.createElement("span");
+    let closeModal = document.createElement("a");
+    let imgModal = document.createElement("img");
+    let aboutModal = document.createElement("div");
+    divModal.style.display = "block";
+    setTimeout(() =>divModal.classList.toggle('fade'),5);
+    closeModal.addEventListener('click',()=>{
+      let modal= document.getElementById("modal");
+      modal.classList.toggle("fade");
+      setTimeout(()=>{
+       modal.style.display = "none";}
+       ,202);
+       divModal.innerHTML = '';
+      });
 
-      fragment.appendChild(tarjeta);
-      
-      doc.appendChild(fragment)
+    divContainerModal.appendChild(spanModal);
+    divContainerModal.appendChild(closeModal);
+    divContainerModal.appendChild(imgModal);
+    divContainerModal.appendChild(aboutModal);
+    fragmentoModal.appendChild(divContainerModal);
+
+    divModal.id = "modal";
+    divContainerModal.id = "containerModal";
+    spanModal.id = "heroName";
+    closeModal.id = "closeModal";
+    aboutModal.id = "about";
+
+    closeModal.textContent = '\u2716';
+    spanModal.textContent = element["name"];
+    imgModal.src = element["picture"];
+    aboutModal.textContent = element["about"];
+
+    divModal.appendChild(fragmentoModal)
+  });
+
+  tarjeta.appendChild(img);
+  nombreHero.appendChild(boton);
+  tarjeta.appendChild(nombreHero);
+  // estructura div.tarjeta > img > div.nombreHero > input.button
+
+  fragment.appendChild(tarjeta);
+  
+  doc.appendChild(fragment)
   });
 
 let cb = document.getElementById('sliderCB')
@@ -61,3 +101,7 @@ function isChecked(){
 isChecked();
 
 cb.addEventListener('change',isChecked);
+
+
+
+
